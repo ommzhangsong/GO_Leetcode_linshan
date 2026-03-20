54fbe6e087bb67f3d00c5b7f3f1a7563ff3693cc
ommzhangsong <zhangsong20021002@163.com>
2026-03-20 22:41:23 +0800

    1

---
+++ 相向双指针/三数之和review/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func threeSum(nums []int) (res [][]int) {
+	sort.Ints(nums)
+	for i := 0; i < len(nums)-2; i++ {
+		if nums[i] > 0 {
+			break
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left := i + 1
+
+		right := len(nums) - 1
+		for left < right {
+			sum := nums[left] + nums[right] + nums[i]
+			if sum < 0 {
+				left++
+			}
+			if sum > 0 {
+				right--
+			}
+			if sum == 0 {
+				res = append(res, []int{nums[left], nums[right], nums[i]})
+				for nums[left] == nums[left+1] && left < right {
+					left++
+				}
+				for nums[right] == nums[right-1] && left < right {
+					right--
+				}
+				left++
+				right--
+			}
+		}
+
+	}
+	return res
+}
+func main() {
+	var a = []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(a))
+
+}
