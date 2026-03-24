2e867b745a8ea250c1c241d0b8b7c4b063556483
ommzhangsong <zhangsong20021002@163.com>
2026-03-24 16:42:30 +0800

    1

---
+++ HOT100/239_滑动窗口最大值/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// 2026.3.24 ,暴力破解, 失败。
+//2. 看答案,完成，待复习
+
+func maxSlidingWindow(nums []int, k int) (ans []int) {
+	q := []int{}
+	for i, v := range nums {
+		for len(q) > 0 && nums[q[len(q)-1]] <= v {
+			q = q[:len(q)-1]
+		}
+		q = append(q, i)
+		if i-k+1 > q[0] {
+			q = q[1:]
+		}
+		if i >= k-1 {
+			ans = append(ans, nums[q[0]])
+		}
+	}
+	return
+}
+func main() {
+	nums := []int{1, 3, 1, 2, 0, 5}
+	k := 3
+	fmt.Println(maxSlidingWindow(nums, k))
+}
